eef1ef0525f28f5792c8d92af27d5180d4fc4efc
Cory Suzuki <6979547+MyPetLobster@users.noreply.github.com>
2024-08-16 12:40:49 -0400

    Refactor city.py, split draw into funcs
    Add config constant for INTERSECTION_TILES

--- city.py
+++ city.py
@@ -1,12 +1,9 @@
 import pygame as pg
-
-from config import ROAD_COLOR, SIDEWALK_COLOR, BLOCK_COLOR, CROSSWALK_COLOR, TILE_SIZE, YELLOW_STRIPE, CROSSWALK_TILES
+from config import ROAD_COLOR, SIDEWALK_COLOR, BLOCK_COLOR, CROSSWALK_COLOR, TILE_SIZE, YELLOW_STRIPE, CROSSWALK_TILES, INTERSECTION_TILES
 
 
 class CityGrid:
     """
-    Class to represent the city grid
-
     Attributes:
         - grid_size (int): The size of the grid
         - grid (list): 2D list representing the city grid
@@ -29,20 +26,23 @@ class CityGrid:
     
     def create_grid(self):
         """
-        Creates a grid of size grid_size x grid_size
-        
+        Creates an empty grid of the given size.
+
         Returns:
-            - list: 2D list representing the city grid
+            list: 2D list representing the city grid, initialized with 'empty' values.
         """
-        grid = [[0 for _ in range(self.grid_size)] for _ in range(self.grid_size)]
-        return grid
+        return [['empty' for _ in range(self.grid_size)] for _ in range(self.grid_size)]
 
     def set_city_elements(self):
         """
-        Sets up the roads and sidewalks on the city grid
+        Sets up the roads and sidewalks on the city grid.
+        """
+        self.set_roads()
+        self.set_sidewalks()
 
-        Returns:
-            None
+    def set_roads(self):
+        """
+        Marks the road tiles on the grid.
         """
         for i in range(self.grid_size):
             self.grid[11][i] = 'road'
@@ -50,59 +50,72 @@ class CityGrid:
             self.grid[12][i] = 'road'
             self.grid[i][12] = 'road'
 
-        # Set up sidewalks
+    def set_sidewalks(self):
+        """
+        Marks the sidewalk tiles on the grid.
+        """
         for i in range(self.grid_size):
             self.grid[10][i] = 'sidewalk'
             self.grid[i][10] = 'sidewalk'
             self.grid[13][i] = 'sidewalk'
             self.grid[i][13] = 'sidewalk'
-            
+
     def draw(self, win):
         """
-        Draws the city grid on the screen
+        Draws the city grid on the screen.
 
         Args:
-            - win (Surface): The pygame window to draw on
-
-        Returns:
-            None
+            win (Surface): The pygame window to draw on.
         """
         for i in range(self.grid_size):
             for j in range(self.grid_size):
-                if self.grid[i][j] == 'road' or self.grid[i][j] == 'occupied':
-                    color = ROAD_COLOR
-                elif self.grid[i][j] == 'sidewalk':
-                    color = SIDEWALK_COLOR
-                else:
-                    color = BLOCK_COLOR
-                
-                # Decouple 'name' and color for crosswalks. Need to use "occupied" in crosswalk tiles to control intersection
-                if (i, j) in self.crosswalks:
-                    color = CROSSWALK_COLOR
+                self.draw_tile(win, i, j)
 
-                pg.draw.rect(win, color, (j * TILE_SIZE, i * TILE_SIZE, TILE_SIZE, TILE_SIZE))
+    def draw_tile(self, win, i, j):
+        """
+        Draws an individual tile on the grid.
 
-                # Draw yellow line between road tiles
-                if self.grid[i][j] == 'road' or (self.grid[i][j] == 'occupied' and (i, j) not in self.crosswalks):
-                    # Exclude the intersection tiles
-                    if (i, j) not in [(11, 11), (11, 12), (12, 11), (12, 12)]:
-                        # Horizontal roads (between rows 11 and 12)
-                        if i == 12:
-                            pg.draw.line(win, YELLOW_STRIPE, (j * TILE_SIZE, i * TILE_SIZE), ((j + 1) * TILE_SIZE, i * TILE_SIZE))
-                        # Vertical roads (between columns 11 and 12)
-                        if j == 12:
-                            pg.draw.line(win, YELLOW_STRIPE, (j * TILE_SIZE, i * TILE_SIZE), (j * TILE_SIZE, (i + 1) * TILE_SIZE))
+        Args:
+            win (Surface): The pygame window to draw on.
+            i (int): The row index of the tile.
+            j (int): The column index of the tile.
+        """
+        if self.grid[i][j] in ['road', 'occupied']:
+            color = ROAD_COLOR
+        elif self.grid[i][j] == 'sidewalk':
+            color = SIDEWALK_COLOR
+        else:
+            color = BLOCK_COLOR
+
+        # Use crosswalk color if the tile is a crosswalk
+        if (i, j) in self.crosswalks:
+            color = CROSSWALK_COLOR
+
+        pg.draw.rect(win, color, (j * TILE_SIZE, i * TILE_SIZE, TILE_SIZE, TILE_SIZE))
+
+        # Draw yellow line between road tiles, except at intersections
+        if self.grid[i][j] in ['road', 'occupied'] and (i, j) not in self.crosswalks and (i, j) not in INTERSECTION_TILES:
+            self.draw_yellow_stripe(win, i, j)
+
+    def draw_yellow_stripe(self, win, i, j):
+        """
+        Draws a yellow stripe on the road tiles.
+
+        Args:
+            win (Surface): The pygame window to draw on.
+            i (int): The row index of the tile.
+            j (int): The column index of the tile.
+        """
+        if i == 12:  # Horizontal roads (between rows 11 and 12)
+            pg.draw.line(win, YELLOW_STRIPE, (j * TILE_SIZE, i * TILE_SIZE), ((j + 1) * TILE_SIZE, i * TILE_SIZE))
+        if j == 12:  # Vertical roads (between columns 11 and 12)
+            pg.draw.line(win, YELLOW_STRIPE, (j * TILE_SIZE, i * TILE_SIZE), (j * TILE_SIZE, (i + 1) * TILE_SIZE))
                         
     def add_traffic_light(self, traffic_light):
         """
-        Adds a traffic light to the city grid
+        Adds a traffic light to the city grid.
 
         Args:
-            - traffic_light (TrafficLight): The traffic light to add
-
-        Returns:
-            None
+            traffic_light (TrafficLight): The traffic light to add.
         """
         self.traffic_lights.append(traffic_light)
-
-
